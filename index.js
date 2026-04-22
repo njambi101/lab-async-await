0af5e42f3ea2cfe1da328145b3920de9d0389a39
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-22 16:57:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const newdiv =document.createElement('h1');
+/*const newdiv =document.createElement('h1');
 const newdiv2 = document.createElement('p')
 
 
@@ -73,4 +73,28 @@ async function fetchPosts() {
 }
 
 // Call the async fetch function
-fetchPosts();
+fetchPosts();*/
+fetch ('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts =>
+        displayPosts(posts)
+    )
+    .catch(error => 
+        {console.error('Error fetching user data:', error);})
+
+const displayPosts = (posts) => {
+    posts.forEach(post => {
+
+        const list = document.createElement('li');
+        const title = document.createElement('h1');
+        title.textContent = post.title;
+        const body = document.createElement('p');
+        body.textContent = post.body;
+
+        list.append(title, body);
+
+        const postList = document.querySelector('#post-list');
+        postList.append(list);
+    }
+    )
+}
