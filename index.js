8ce6f8ef03b931b56a5959c4cc7b3b174f41925b
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-22 14:30:37 +0300

    saved

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+//const inputelm = document.getElementById('title1')
+//const infotext = document.getElementById('bodypost')
+const newdiv =document.createElement('title1');
+const newdiv2 = document.createElement('bodypost')
+
+
+
+async function fetchApi(word){
+    try{
+        const url ='https://jsonplaceholder.typicode.com/posts'
+        const result = await fetch(url).then((res)=> res.json())
+       newdiv.innerText = result[0].title;
+       newdiv2.innerText = result[0].body;
+       
+    }
+    catch(error){
+        console.log(error)
+    }
+    console.log(url)
+}
