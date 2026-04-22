c3dc88a8cfa6eb2bf810b02083ecac26ee2a3f8e
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-22 14:59:59 +0300

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const newdiv =document.createElement('title1');
-const newdiv2 = document.createElement('bodypost')
+const newdiv =document.createElement('h1');
+const newdiv2 = document.createElement('p')
 
 
-const inputelm = document.getElementById('title1')
-const infotext = document.getElementById('bodypost')
+//const inputelm = document.getElementById('title1')
+//const infotext = document.getElementById('bodypost')
 
 
 const NewDeve = document.getElementById("post-list");
@@ -16,10 +16,21 @@ async function fetchApi(word){
         const result = await fetch(url).then((res)=> res.json())
        newdiv.innerText = result[0].title;
        newdiv2.innerText = result[0].body;
+
+        //container.append(newdiv);
+        //container.append(newdiv2);
+          newdiv.innerText = result[0].title;
+        newdiv2.innerText = result[0].body;
+
+        // Change 'container' to 'NewDeve' to match your variable name
+        if (NewDeve) {
+            NewDeve.append(newdiv);
+            NewDeve.append(newdiv2);
+        }
        
+    } catch (error) {
+        console.log(error);
     }
-    catch(error){
-        console.log(error)
-    }
-    console.log(url)
 }
+  
+fetchApi();
